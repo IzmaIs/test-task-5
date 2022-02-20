f24c5d04047e90c317ca97ef3506fa6cad09a8f7
Ihsan <ihsan9985@yandex.ru>
2022-02-20 08:21:44 +0300

    Upload сообщение ошибки

--- src/components/Fio.js
+++ src/components/Fio.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import {FioSuggestions} from "react-dadata";
-
+import 'react-dadata/dist/react-dadata.css';
 
 export default function Fio (props) {
     const token = "554a1ccc6c63554d7da56fe0b549733c18a46c14";
@@ -12,7 +12,7 @@ export default function Fio (props) {
                 token={token}
                 value={props.value}
                 onChange={props.onChange}
-                inputProps={{className: `input ${props.error ? "input-error" : ""}`}}
+                inputProps={{className: `react-dadata__input ${props.error ? "input-error" : ""}`}}
             />
             <span className="input-error-msg">{props.error ? "Заполните поле ФИО" : " "}</span>
         </>
